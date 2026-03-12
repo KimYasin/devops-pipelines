cccab9267cc42272fe2fa0d0408a7e0383fb583a
KIM <kim@hrpsolutions.com>
2026-03-12 18:30:08 +0300

    feat: add  CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello, World!');
+console.log('This is a simple Node.js application, I am learning CI/CD with GitHub Actions.');
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('Starting the tests...');
+setTimeout(() => console.log('Waiting for 3 minutes...'), 3000);
+console.log('Tests completed successfully!');
